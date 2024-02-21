269227d5af39aea99a0347838520dc89f31396c3
Nathaniel Hartlen <nwhartlen@icloud.com>
2024-02-21 13:43:47 -0400

    shader support

--- editor.py
+++ editor.py
@@ -6,11 +6,16 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 from pyopengltk import OpenGLFrame
 from OpenGL import GL, GLU
+import OpenGL.GL.shaders
+import ctypes
+import types
+import numpy
 import os
 import glob
 import gc
 import platform
 import shutil #not used
+import time
 
 # Some global configurations
 global_ver = "0.14"
@@ -51,6 +56,37 @@ def paste():
     pass
 def rename():
     pass
+def compileShader(source, shaderType):
+    """
+    uses code from: https://github.com/jonwright/pyopengltk/blob/master/examples/shader_example.py
+    Compile shader source of given type
+        source -- GLSL source-code for the shader
+    shaderType -- GLenum GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, etc,
+        returns GLuint compiled shader reference
+    raises RuntimeError when a compilation failure occurs
+    """
+    if isinstance(source, str):
+        source = [source]
+    elif isinstance(source, bytes):
+        source = [source.decode('utf-8')]
+
+    shader = GL.glCreateShader(shaderType)
+    GL.glShaderSource(shader, source)
+    GL.glCompileShader(shader)
+    result = GL.glGetShaderiv(shader, GL.GL_COMPILE_STATUS)
+    if not(result):
+        # TODO: this will be wrong if the user has
+        # disabled traditional unpacking array support.
+        raise RuntimeError(
+            """Shader compile failure (%s): %s""" % (
+                result,
+                GL.glGetShaderInfoLog(shader),
+            ),
+            source,
+            shaderType,
+        )
+    return shader
+
 logwrite("KUNITY logfile --- \\/\n---------------------")
 
 def populate_tree(tree, node, parent=""):
